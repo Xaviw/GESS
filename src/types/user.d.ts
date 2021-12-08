0bff4d1b4c37ddffc5bd899bc83b9062d2c34945
XRain <704466069@qq.com>
2021-12-08 23:34:34 +0800

    login pager

---
+++ src/types/user.d.ts
@@ -0,0 +1,5 @@
+export declare interface UserForm {
+  account: string;
+  password: string;
+  confirm?: string;
+}
--- src/views/login.vue
+++ src/views/login.vue
@@ -15,37 +15,46 @@
       <p class="mb-3 text-3xl font-medium">考研分享平台</p>
       <p class="text-gray-500 mb-8">用户登录</p>
       <a-form
-        :model="formState"
-        @finish="handleFinish"
-        @finishFailed="handleFinishFailed"
+        ref="formRef"
+        :model="loginForm"
+        :rules="loginRules"
+        @finish="handleLogin"
         scrollToFirstError="true"
         class="w-full"
-        size="medium"
       >
-        <a-form-item hasFeedback required>
-          <a-input v-model:value="formState.user" placeholder="用户名">
+        <a-form-item hasFeedback name="account">
+          <a-input
+            v-model:value="loginForm.account"
+            placeholder="用户名"
+            autocomplete="off"
+          >
             <template #prefix
               ><UserOutlined style="color: rgba(0, 0, 0, 0.25)"
             /></template>
           </a-input>
         </a-form-item>
-        <a-form-item hasFeedback required>
+        <a-form-item hasFeedback name="password">
           <a-input
-            v-model:value="formState.password"
+            v-model:value="loginForm.password"
             type="password"
             placeholder="密码"
+            autocomplete="off"
           >
             <template #prefix
               ><LockOutlined style="color: rgba(0, 0, 0, 0.25)"
             /></template>
           </a-input>
         </a-form-item>
+        <div class="flex justify-between mb-4">
+          <a-checkbox v-model:checked="rememberLogin">记住登录状态</a-checkbox>
+          <span>
+            <a href="">忘记密码</a>
+            |
+            <a href="">注册账号</a>
+          </span>
+        </div>
         <a-form-item class="text-center">
-          <a-button
-            type="primary"
-            html-type="submit"
-            :disabled="formState.user === '' || formState.password === ''"
-          >
+          <a-button type="primary" html-type="submit" class="w-full">
             登录
           </a-button>
         </a-form-item>
@@ -56,26 +65,40 @@
 
 <script setup lang="ts">
 import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
-import { ValidateErrorEntity } from "ant-design-vue/es/form/interface";
-import { defineComponent, reactive, UnwrapRef } from "vue";
-interface FormState {
-  user: string;
-  password: string;
-}
+import { ref, reactive, UnwrapRef } from "vue";
+import { UserForm } from "@/types/user";
 
-const formState: UnwrapRef<FormState> = reactive({
-  user: "",
+const formRef = ref();
+const loginForm: UnwrapRef<UserForm> = reactive({
+  account: "",
   password: "",
 });
-const handleFinish = (values: FormState) => {
-  console.log(values, formState);
+const loginRules = {
+  account: [{ required: true, trigger: "blur", message: "请输入账号" }],
+  password: [
+    { required: true, message: "请输入密码", trigger: "blur" },
+    {
+      min: 6,
+      max: 16,
+      message: "密码长度应为6-16位",
+      trigger: "blur",
+    },
+    {
+      pattern: /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z_]{6,16}$/,
+      trigger: "blur",
+      message: "密码应至少包含一个字母和一个数字",
+    },
+  ],
 };
-const handleFinishFailed = (errors: ValidateErrorEntity<FormState>) => {
-  console.log(errors);
+const handleLogin = (values: UserForm) => {
+  console.log(values, loginForm);
+  // loginForm.validate();
 };
+
+let rememberLogin = ref(false);
 </script>
 
-<style scoped>
+<style>
 .login-page {
   background-image: url("@/assets/login-background.jpg");
 }
