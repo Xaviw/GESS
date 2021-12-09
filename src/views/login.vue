ca596f48073386b77affa13795c8b52e24fe2c4a
XW <974485374@qq.com>
2021-12-09 17:53:53 +0800

    login page style done

--- src/views/login.vue
+++ src/views/login.vue
@@ -10,12 +10,13 @@
         blur-20px
         flex flex-col
         items-center
+        <mobile:(w-full)
       "
     >
       <p class="mb-3 text-3xl font-medium">考研分享平台</p>
       <p class="text-gray-500 mb-8">用户登录</p>
       <a-form
-        ref="formRef"
+        :ref="formRef"
         :model="loginForm"
         :rules="loginRules"
         @finish="handleLogin"
@@ -23,11 +24,7 @@
         class="w-full"
       >
         <a-form-item hasFeedback name="account">
-          <a-input
-            v-model:value="loginForm.account"
-            placeholder="用户名"
-            autocomplete="off"
-          >
+          <a-input v-model:value="loginForm.account" placeholder="用户名">
             <template #prefix
               ><UserOutlined style="color: rgba(0, 0, 0, 0.25)"
             /></template>
@@ -38,24 +35,34 @@
             v-model:value="loginForm.password"
             type="password"
             placeholder="密码"
-            autocomplete="off"
           >
             <template #prefix
               ><LockOutlined style="color: rgba(0, 0, 0, 0.25)"
             /></template>
           </a-input>
         </a-form-item>
-        <div class="flex justify-between mb-4">
-          <a-checkbox v-model:checked="rememberLogin">记住登录状态</a-checkbox>
-          <span>
-            <a href="">忘记密码</a>
-            |
-            <a href="">注册账号</a>
-          </span>
+        <a-form-item hasFeedback name="confirm" v-if="!isLoginState">
+          <a-input
+            v-model:value="loginForm.confirm"
+            type="password"
+            placeholder="确认密码"
+          >
+            <template #prefix
+              ><LockOutlined style="color: rgba(0, 0, 0, 0.25)"
+            /></template>
+          </a-input>
+        </a-form-item>
+        <div class="flex flex-row-reverse justify-between mb-4">
+          <a @click="changePageState">{{
+            isLoginState ? "注册账号" : "前往登录"
+          }}</a>
+          <a-checkbox v-model:checked="rememberLogin" v-show="isLoginState"
+            >记住登录状态</a-checkbox
+          >
         </div>
         <a-form-item class="text-center">
           <a-button type="primary" html-type="submit" class="w-full">
-            登录
+            {{ isLoginState ? "登录" : "注册" }}
           </a-button>
         </a-form-item>
       </a-form>
@@ -66,36 +73,65 @@
 <script setup lang="ts">
 import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
 import { ref, reactive, UnwrapRef } from "vue";
-import { UserForm } from "@/types/user";
+import type { RuleObject } from "ant-design-vue/es/form";
+import type { UserForm } from "@/types/user";
+import http from "@/apis/request";
 
+// 登录注册状态
+let isLoginState = ref(true);
+function changePageState() {
+  loginForm.password = loginForm.confirm = "";
+  isLoginState.value = !isLoginState.value;
+}
+
+// 记住登录状态
+let rememberLogin = ref(false);
+
+// 密码验证器
+let validatePass = async (_rule: RuleObject, value: string) => {
+  if (value === "") {
+    return Promise.reject("请输入密码");
+  } else {
+    if (value.length < 6 || value.length > 16) {
+      return Promise.reject("密码长度应为6-16位");
+    }
+    if (!/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z_]{6,16}$/.test(value)) {
+      return Promise.reject("密码应至少包含一个数字和字母");
+    }
+    if (loginForm.confirm !== "") {
+      formRef.value.validateFields("confirm");
+    }
+    return Promise.resolve();
+  }
+};
+let validatePass2 = async (_rule: RuleObject, value: string) => {
+  if (value === "") {
+    return Promise.reject("请再次输入密码");
+  } else if (value !== loginForm.password) {
+    return Promise.reject("两次输入不一致");
+  } else {
+    return Promise.resolve();
+  }
+};
+
+// 表单
 const formRef = ref();
 const loginForm: UnwrapRef<UserForm> = reactive({
   account: "",
   password: "",
+  confirm: "",
 });
 const loginRules = {
   account: [{ required: true, trigger: "blur", message: "请输入账号" }],
-  password: [
-    { required: true, message: "请输入密码", trigger: "blur" },
-    {
-      min: 6,
-      max: 16,
-      message: "密码长度应为6-16位",
-      trigger: "blur",
-    },
-    {
-      pattern: /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z_]{6,16}$/,
-      trigger: "blur",
-      message: "密码应至少包含一个字母和一个数字",
-    },
-  ],
+  password: [{ validator: validatePass, trigger: "change" }],
+  confirm: [{ validator: validatePass2, trigger: "change" }],
 };
 const handleLogin = (values: UserForm) => {
-  console.log(values, loginForm);
-  // loginForm.validate();
+  if (isLoginState.value) {
+    http.post("/login", values).then((res) => {});
+  } else {
+  }
 };
-
-let rememberLogin = ref(false);
 </script>
 
 <style>
--- windi.config.ts
+++ windi.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite-plugin-windicss";
 
 export default defineConfig({
-  attributify: true,
   extract: {
     include: ["src/**/*.{vue,html}"],
     exclude: ["node_modules", ".git"],
   },
   plugins: [require("@windicss/plugin-scrollbar")],
+  theme: {
+    screens: {
+      mobile: "425px",
+    },
+  },
 });
